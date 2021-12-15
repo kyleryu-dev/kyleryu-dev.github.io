db8476c55a55ea0069ff51ff0d8c798dacd2b9eb
Kyle Ryu <85800995+kyleryu-dev@users.noreply.github.com>
2021-12-15 14:42:50 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,5 +5,12 @@ $(function() {
     $('#name').css('opacity','1');
   },1800);
 
+  $(window).scroll(function() {
+    if($(window).scrollTop() > 2800) {
+      $('#scroll_bar').css('animation-direction','reverse');    
+    } else {
+      $('#scroll_bar').css('animation-direction','normal');
+    }
+  });
 
 });
