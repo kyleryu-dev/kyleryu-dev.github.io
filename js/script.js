7e7c1ea56d03cf2ba1cc0330876c1bb2d6e30bf5
Kyle Ryu <85800995+kyleryu-dev@users.noreply.github.com>
2021-12-16 11:33:36 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,13 +6,19 @@ $(function() {
   },1800);
 
   $(window).scroll(function() {
+
+    /* height 추출 */
+    var height = $(window).scrollTop();
+    console.log(height);
+
+    /* pc scroll_bar Animation */
     if($(window).scrollTop() > 2800) {
       $('#scroll_bar').css('animation-direction','reverse');    
     } else {
       $('#scroll_bar').css('animation-direction','normal');
     }
 
-    // media Query
+    /* media Query */
     var windowWidth = window.matchMedia("screen and (max-width: 768px)");
     if (windowWidth.matches) {
       if($(window).scrollTop() > 2000) {
@@ -21,6 +27,7 @@ $(function() {
         $('#scroll_bar').css('animation-direction','normal');
       }
     }
+
   });
 
 
