b8548f5cbe7301c81b2c8c562294d70ba49d4f45
Kyle Ryu <85800995+kyleryu-dev@users.noreply.github.com>
2021-12-16 14:48:21 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -11,8 +11,19 @@ $(function() {
     var height = $(window).scrollTop();
     console.log(height);
 
-    var y =  -1/500 * height + 115/50;
-    $('.wrap_box').eq(0).css('opacity', y);
+    /* section opacity */
+    var a =  -1/934 * height + 1;
+    $('.wrap_box').eq(0).css('opacity', a);
+
+    var b =  -1/938 * height + 936/469;
+    $('.wrap_box').eq(1).css('opacity', b);
+
+    var c =  -1/939 * height + 937/313;
+    $('.wrap_box').eq(2).css('opacity', c);
+
+    /* section first scale */
+    var x = (-1/9340) * height + 1;
+    $('.wrap_box').eq(0).css('transform', `scale( ${x} )`);
 
     /* pc scroll_bar Animation */
     if($(window).scrollTop() > 2800) {
@@ -24,11 +35,30 @@ $(function() {
     /* media Query */
     var windowWidth = window.matchMedia("screen and (max-width: 768px)");
     if (windowWidth.matches) {
+
+      /* mobile scroll_bar Animation */
       if($(window).scrollTop() > 2000) {
         $('#scroll_bar').css('animation-direction','reverse');    
       } else {
         $('#scroll_bar').css('animation-direction','normal');
       }
+
+      /* mobile section opacity */
+      var a =  -1/668 * height + 1;
+      $('.wrap_box').eq(0).css('opacity', a);
+
+      var b =  -1/666 * height + 445/222;
+      $('.wrap_box').eq(1).css('opacity', b);
+
+      var c =  -1/665 * height + 2001/665;
+      $('.wrap_box').eq(2).css('opacity', c);
+
+      /* section first scale */
+      var x = (-1/6680) * height + 1;
+      $('.wrap_box').eq(0).css('transform', `scale( ${x} )`);
+
+
+
     }
 
   });
