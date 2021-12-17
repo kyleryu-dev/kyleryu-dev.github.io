bbfaae13ac1b995c20ba71aad24d8d7e85d48df5
Kyle Ryu <85800995+kyleryu-dev@users.noreply.github.com>
2021-12-17 15:58:00 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $(function() {
 
     /* height 추출 */
     var height = $(window).scrollTop();
-    // console.log(height);
+    console.log(height);
 
     /* section opacity */
     var a = -1/934 * height + 1;
@@ -32,17 +32,64 @@ $(function() {
       $('#scroll_bar').css('animation-direction','normal');
     }
 
-    /* media Query - tablet */
-    var windowWidth = window.matchMedia("screen and (max-width: 1200px)");
+    /* media Query - Ipad pro */
+    var windowWidth = window.matchMedia("screen and (max-width: 1024px)");
     if (windowWidth.matches) {
 
+      /* tablet scroll_bar Animation */
+      if($(window).scrollTop() > 4080) {
+        $('#scroll_bar').css('animation-direction','reverse');    
+      } else {
+        $('#scroll_bar').css('animation-direction','normal');
+      }
 
+      /* tablet section opacity */
+      var a = -1/1366 * height + 1;
+      $('.wrap_box').eq(0).css('opacity',a);
+
+      var b = -1/1004 * height + 1185/502;
+      $('.wrap_box').eq(1).css('opacity',b);
+
+      var c = -1/1368 * height + 683/228;
+      $('.wrap_box').eq(2).css('opacity',c);
+
+      /* section1 scale */
+      var x = (-1/6680) * height + 1;
+      $('.wrap_box').eq(0).css('transform','scale( ${x} )');
 
     }
-    /* //media Query - tablet */
+    /* //media Query - Ipad pro */
+
+    /* media Query - Ipad */
+    var windowWidth = window.matchMedia("screen and (max-width: 768px)");
+    if (windowWidth.matches) {
+
+      /* tablet scroll_bar Animation */
+      if($(window).scrollTop() > 3070) {
+        $('#scroll_bar').css('animation-direction','reverse');    
+      } else {
+        $('#scroll_bar').css('animation-direction','normal');
+      }
+
+      /* tablet section opacity */
+      var a = -1/1024 * height + 1;
+      $('.wrap_box').eq(0).css('opacity',a);
+
+      var b = -1/1022 * height + 1023/511;
+      $('.wrap_box').eq(1).css('opacity',b);
+
+      var c = -1/1026 * height + 512/171;
+      $('.wrap_box').eq(2).css('opacity',c);
+
+      /* section1 scale */
+      var x = (-1/6680) * height + 1;
+      $('.wrap_box').eq(0).css('transform','scale( ${x} )');
+
+    }
+    /* //media Query - Ipad */
 
     /* media Query - mobile */
-    var windowWidth = window.matchMedia("screen and (max-width: 768px)");
+    var windowWidth = window.matchMedia("screen and (max-width: 576px)");
     if (windowWidth.matches) {
 
       /* mobile scroll_bar Animation */
